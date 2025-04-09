ba2101578d1c0a7f9155edde9e72ca0df9063a9f
mehdiHIRCH <Mehdihirch6@gmail.com>
2025-04-09 21:09:57 +0100

    deploy 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/levelUp-shool-website/',  // Add this line to set the base path for GitHub Pages
   server: {
     host: true,
     port: 5173,
