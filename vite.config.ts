c7c63e3efde59b00fb667d37f2754957572c8b6f
mehdiHIRCH <Mehdihirch6@gmail.com>
2025-04-10 18:51:32 +0100

    redeploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'development' ? '/' : '/levelUp-shool-website/',
+  // base: mode === 'development' ? '/' : '/levelUp-shool-website/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
